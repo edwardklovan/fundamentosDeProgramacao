d7a1a8879107bdd05e5216170041887f33655a7f
Edward <github@kln.me>
2016-03-21 20:32:33 -0300

    terminando exercício

--- ExerciciosAula/NumNegativo.java
+++ ExerciciosAula/NumNegativo.java
@@ -2,6 +2,27 @@ import java.util.Scanner;
 
 public class NumNegativo {
     public static void main(String[] args) {
-        
+
+        // variáveis
+        int n, contador, continuar;
+
+        // inicalizar incremento
+        contador = 0;
+
+        Scanner entrada = new Scanner(System.in);
+
+        do {
+            System.out.println("Escreva um número.");
+            n = entrada.nextInt();
+
+            if (n < 0) {
+                contador++;
+            }
+            System.out.println("Digite 1 para continuar");
+            continuar = entrada.nextInt();
+
+        } while (continuar == 1);
+
+        System.out.println("Você digitou " + contador + " núneros negativos.");
     }
 }
