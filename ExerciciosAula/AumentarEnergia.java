694352d4b6c9fec91ab22945a70090339953c7c0
Edward Klovan <edwardklovan@users.noreply.github.com>
2016-03-28 13:01:41 -0300

    Enviando exercício realizado no laboratório.

---
+++ ExerciciosAula/AumentarEnergia.java
@@ -0,0 +1,70 @@
+/**
+ * @author Edward
+ * @version 28/03/2016
+
+ *
+ */
+
+import java.util.Scanner;
+
+
+public class AumentarEnergia {
+    public static void man(String[] args) {
+
+        // Declaracao de variaveis
+        int codCentral1, codCentral2, codCentral3;
+        int qeCentral1, qeCentral2, qeCentral3;
+        int codigoCentral, novoValor;
+        
+        codCentral1 = 50;
+        codCentral2 = 51;
+        codCentral3 = 52;
+                
+        qeCentral1 = 0;
+        qeCentral2 = 0;
+        qeCentral3 = 0;
+        
+        novoValor = 1;
+        
+        // entrada de dados
+        Scanner entrada = new Scanner(System.in); 
+        
+        System.out.println("Digite o código da central que deseja alterar. Para sair digite 0");
+        codigoCentral = entrada.nextInt();
+                
+        while (codigoCentral != 0) {
+              
+                System.out.println("Digite o novo valor.");
+                novoValor = entrada.nextInt();
+                
+                if (codigoCentral == codCentral1) {
+                    qeCentral1 = novoValor;
+                }
+            
+                if (codigoCentral == codCentral2) {
+                    qeCentral2 = novoValor;
+                }
+            
+                if (codigoCentral == codCentral3) {
+                    qeCentral3 = novoValor;
+                }
+                
+                 System.out.println("\n");
+                 System.out.println("Centrais \n");
+                 System.out.println("Central " + codCentral1 + ":" + qeCentral1 + "MW");
+                 System.out.println("Central " + codCentral2 + ":" + qeCentral2 + "MW");
+                 System.out.println("Central " + codCentral3 + ":" + qeCentral3 + "MW");
+                 System.out.println("\n");
+                 
+                System.out.println("Digite o código da central que deseja alterar. Para sair digite 0");
+                codigoCentral = entrada.nextInt();
+                    
+            }
+          
+        if (codigoCentral == 0) {
+            System.out.println("Você saiu do programa.");
+        }
+               
+
+    }
+}
