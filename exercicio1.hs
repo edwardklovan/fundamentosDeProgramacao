b44f1a0df7d3fbd0379a9e287f372ec22cd9a707
Edward Klovan <edwardklovan@users.noreply.github.com>
2016-03-31 11:32:20 -0300

    Adicionado fatoração

--- exercicio1.hs
+++ exercicio1.hs
@@ -2,4 +2,12 @@
 soma::(Integer,Integer) -> Integer
 
 soma (0, n) = n
-soma (m,n) = soma(m-1, n+1)  
+soma (m,n) = soma(m-1, n+1)
+
+
+-- Fatorial de um inteiro
+
+fat::Integer -> Integer
+
+fat(0) = 1
+fat(n) = n * fat(n-1)   
